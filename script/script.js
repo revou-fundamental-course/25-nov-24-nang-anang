121e84866fbf289cd2d84bfe1e0f02a67ee43b33
nang-anang <amirudin.anang@gmail.com>
2024-12-05 19:28:44 +0700

    update css dan js

--- script/script.js
+++ script/script.js
@@ -1,21 +1,43 @@
-function validateForm() {
-    let inputBeratBadan = document.getElementById('berat').value;
-    let inputUsia = document.getElementById('usia').value;
-    let inputTinggiBadan = document.getElementById('tinggi').value;
+const bmiText = document.getElementById('result');
+const descStatus = document.getElementById('status');
+const form = document.querySelector('form');
 
-    console.log('BB:' + inputBeratBadan);
-    console.log('Usia:' + inputUsia);
-    console.log('Tinggi:' + inputTinggiBadan);
+form.addEventListener("submit", onFormSubmit);
 
-    if (inputBeratBadan != '' && inputTinggiBadan != '' && inputUsia != ''){
-        let calculate = parseInt (inputBeratBadan) + parseInt (inputTinggiBadan);
-        updateResult(calculate);
-    }else{
-        alert("Iputan kosong!!");
+function onFormSubmit(e){
+    e.preventDefault();
+
+    const berat = parseFloat(form.berat.value);
+    const tinggi = parseFloat(form.tinggi.value);
+    const usia = parseInt(form.usia.value);
+
+    if (isNaN(berat)|| isNaN(tinggi) ||isNaN(usia)|| berat <=0 || tinggi <=0 || usia <=0 ){
+        alert ("Mohon masukkan data dengan benar!");
+        return;
     }
 
+    const tinggiDlmMeter = tinggi/100; /*konversi tinggi bada cm -> m*/
+    const bmi = berat/ Math.pow(tinggiDlmMeter,2); /*rumus BMI*/
+    const desc = kriteriaBMI (bmi);
+
+    bmiText.textContent = bmi.toFixed(1);
+    descStatus.innerHTML = desc;
 }
 
-function updateResult (value) {
-    document.getElementById('result').innerHTML = value;
+// fungsi if untuk kriteria BMI
+function kriteriaBMI(bmi){
+    if(bmi < 18.5){
+        return 'Kurang berat badan';
+    }else if(bmi < 25){
+        return 'Normal (Ideal)';
+    }else if (bmi < 30){
+        return 'Kelebihan berat badan';
+    }else{
+        return 'Obesitas';
+    }
+}
+
+function reset(){
+    document.getElementById('result').value.reset();
+    document.getElementById('status').value.reset();
 }
